97e435604e00b8268e6612cb9774a11b782a8f2f
Jesper Bratt <xumbik@gmail.com>
2014-10-14 10:17:18 +0200

    Portfolio progress

--- bratfolio/partials/portfolio.js
+++ bratfolio/partials/portfolio.js
@@ -4,5 +4,9 @@ angular.module('bratfolio.portfolio', [])
 
 .controller('PortfolioCtrl', ['$scope', '$routeParams', '$http', 
 	function($scope, $routeParams, $http) {
+		$http.get('data/projects.json').success( function(data) {
+			$scope.projects = data;
+		});
+		
 		$scope.projectId = $routeParams.projectId;
 }]);
