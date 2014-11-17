74fdc0c6c053849ee41bad734edba9ec5202a477
Jesper Bratt <xumbik@gmail.com>
2014-11-17 13:24:47 +0100

    Presentation, CV and project view left.

--- bratfolio/main.js
+++ bratfolio/main.js
@@ -4,7 +4,7 @@
 angular.module('bratfolio', [
   'ngRoute',
   'ngAnimate',
-  'bratfolio.portfolio'
+  'duScroll'
   ])
 
 .config(['$routeProvider', function ($routeProvider) {
@@ -19,7 +19,7 @@ angular.module('bratfolio', [
     })
     .when('/cv', {
         templateUrl: 'partials/cv.html',
-        controller: 'MainCtrl'
+        controller: 'CVCtrl'
     })
     .when('/portfolio', {
         templateUrl: 'partials/portfolio.html',
@@ -34,9 +34,38 @@ angular.module('bratfolio', [
     });
 }])
 
-.controller('MainCtrl', [function() {
+.controller('MainCtrl', ['$scope', function($scope) {
 
+}])
+
+.controller('CVCtrl', ['$scope', function($scope) {
+
+}])
+
+.controller('PortfolioCtrl', ['$scope', '$http', 
+    function($scope, $http) {
+        $http.get('data/projects.json').success( function(data) {
+            $scope.projects = data;
+        });
+
+        $http.get('http://baconipsum.com/api/?type=all-meat&paras=1').success( function(data) {
+            $scope.bacon = data;
+        });
+}])
+
+.controller('ProjectCtrl', ['$scope', '$routeParams', '$http', 
+    function($scope, $routeParams, $http) {
+        $http.get('data/projects/' + $routeParams.projectId + '.json').success(function(data) {
+            console.log(data);
+            $scope.project = data;
+        });
+        
+        $scope.projectId = $routeParams.projectId;
 }]);
+
+function expandInfo(el) {
+    $(el).find('.extra-info').slideToggle("slow");
+};
 
 // Making the menu stick
 function stickyRelocate() {
@@ -44,12 +73,26 @@ function stickyRelocate() {
     var div_top = $('#sticky-anchor').offset().top;
     if (window_top > div_top) {
         $('#sticky').addClass('stick').width($(window).width());
-        $('#sticky-anchor').css("padding", "40px");
+        $('#sticky-cv').addClass('stick').width($(window).width());
+        $('#sticky-anchor').css("padding", "35px");
+        $('#sticky-cv-anchor').css("padding", "28px");
+        $('#top-scroll').css("opacity", "1");
+        $('#perm-top-scroll').css({
+            "bottom": "-20px",
+            "opacity": "1"
+        });
     } else {
         $('#sticky').removeClass('stick');
+        $('#sticky-cv').removeClass('stick');
         $('#sticky-anchor').css("padding", "0px");
+        $('#sticky-cv-anchor').css("padding", "0px");
+        $('#top-scroll').css("opacity", "0");
+        $('#perm-top-scroll').css({
+            "bottom": "-60px",
+            "opacity": "0"
+        });
     }
-}
+};
 
 $(function () {
     $(window).scroll(stickyRelocate);
@@ -59,5 +102,6 @@ $(function () {
 $(function () {
     $(window).resize(function() {
         $('#sticky').css("width", "100%");
+        $('#sticky-cv').css("width", "100%");
     });
 });
