c969590868e8d0805182e8d555dcd4626fba9e2d
Jesper Bratt <jesper.bratt@findwise.com>
2016-09-13 18:27:15 +0200

    Splash update

--- bratfolio/utils/particles.js
+++ bratfolio/utils/particles.js
@@ -5,7 +5,7 @@
 var config = {
   "particles": {
     "number": {
-      "value": 150,
+      "value": 120,
       "density": {
         "enable": true,
         "value_area": 800
@@ -27,30 +27,30 @@ var config = {
       "anim": {
         "enable": true,
         "speed": 1,
-        "opacity_min": 0.5,
+        "opacity_min": 0.1,
         "sync": false
       }
     },
     "size": {
-      "value": 4,
+      "value": 3,
       "random": true,
       "anim": {
         "enable": false,
-        "speed": 10,
+        "speed": 4,
         "size_min": 0.3,
         "sync": false
       }
     },
     "line_linked": {
-      "enable": false,
+      "enable": true,
       "distance": 150,
       "color": "#ffffff",
-      "opacity": 0.4,
+      "opacity": 0.2,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 4,
+      "speed": 2,
       "direction": "none",
       "random": true,
       "straight": false,
